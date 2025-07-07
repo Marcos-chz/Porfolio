ef639e143a87252ee144a80b0a3f0a1f46338980
Marcos Chávez <metkisim@gmail.com>
2025-07-07 18:07:46 -0300

    Porfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <header className='container-fluid'></header>
+      <div className='container max'>
+        <section className='main row'>
+          <div className='col flex flex-column'>
+            <h1>Marcos Chávez</h1>
+            <h2>Desarollador Web</h2>
+          </div>
+
+          <div className='col flex'>
+            <img className='my-foto' src="https://ellees.h-cdn.co/assets/15/37/640x942/640x942-52-55d8-11e6-8246-1ff36568f40fayer-y-hoy-asi-ha-cambiado-johnny-depp-1993-12849750-1-esl-es-1993-jpg.jpg" alt="" />
+          </div>
+        </section>
+
+        <h1>Proyectos</h1>
+        <section className='proyects container'>
+
+        </section>
+        </div>
     </div>
   );
 }
